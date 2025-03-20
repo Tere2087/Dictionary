ab95a83eea51b29af7322b79240c2756fa2ed3b2
Tere <154341453+Tere2087@users.noreply.github.com>
2025-03-20 10:06:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,5 @@ def eliminar_palabra(palabra):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
 
