147a888f592428346a2cb381674e51fc57067b7f
Tere <154341453+Tere2087@users.noreply.github.com>
2025-03-20 08:46:50 +0000

    Update Railway 20.03.25 app.py

--- app.py
+++ app.py
@@ -57,4 +57,7 @@ def eliminar_palabra(palabra):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
